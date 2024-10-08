db71ebfaa174909108e33cb997a7433dc8f9e35c
rishiidwivedi <127668182+rishiidwivedi@users.noreply.github.com>
2024-10-08 12:15:25 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,6 +1,36 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+    const m = s.length;
+    const p = p.length;
+
+    
+    const dp = Array(m + 1).fill(false).map(() => Array(p + 1).fill(false));
+
+  
+    dp[0][0] = true;
+
+ 
+    for (let j = 1; j <= p; j++) {
+        if (pattern[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= p; j++) {
+            if (pattern[j - 1] === message[i - 1] || pattern[j - 1] === '?') {
+              
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (pattern[j - 1] === '*') {
+            
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            }
+        }
+    }
+
+  
+    return dp[m][p];
 
   };
   
