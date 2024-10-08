dcaeb5c647a37df11a47d1c8db1feaebf70d851f
rishiidwivedi <127668182+rishiidwivedi@users.noreply.github.com>
2024-10-08 12:12:29 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,35 @@
 const getTotalIsles = function (grid) {
-
-
-  // write your code here
+if (grid.length === 0) return 0;
+  
+      let numIslands = 0;
+  
+      function dfs(grid, i, j) {
+          
+          if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
+              return;
+          }
+  
+          
+          grid[i][j] = 'W';
+  
+        
+          dfs(grid, i + 1, j); 
+          dfs(grid, i - 1, j); 
+          dfs(grid, i, j + 1); 
+          dfs(grid, i, j - 1);
+      }
+  
+   
+      for (let i = 0; i < grid.length; i++) {
+          for (let j = 0; j < grid[0].length; j++) {
+              if (grid[i][j] === 'L') {
+                  numIslands++;
+                  dfs(grid, i, j); 
+              }
+          }
+      }
+  
+     return numIslands;
 
 };
 
